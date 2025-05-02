0aa34c87f23318f2b93324432c1e51da2e263119
0dytpq0 <rkfnahs12e@gmail.com>
2025-05-02 16:50:00 +0900

    style : font 설정

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="font-hahmlet text-4xl">폰트 구하는 즁~</h1>
+        <p className="font-hahmlet text-2xl mt-4">Im finding font~</p>
+      </main>
+    </div>
+  );
+}
