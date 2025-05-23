4e4eb4ebac69aaee46c31a636c1dcf86895c490e
Joseph Park <rkfnahs12e@gmail.com>
2025-05-23 23:10:34 +0900

    move : route group 폴더 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Hahmlet, Playwrite_DK_Loopet } from 'next/font/google';
-import '../globals.css';
+import './globals.css';
 
 const hahmlet = Hahmlet({
   variable: '--font-hahmlet',
